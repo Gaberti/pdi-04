2db482684f5ebc14fe6c7950a5038a585b969642
louis-br <46930119+louis-br@users.noreply.github.com>
2022-10-15 20:41:33 +0000

    Test adaptative+global

--- main.py
+++ main.py
@@ -17,14 +17,34 @@ def main():
         if img is None:
             print('Failed to open image. \n')
             sys.exit()
+
+        #img = img.astype (np.float32) / 255
         
-        blur = cv2.bilateralFilter(img, 11, 85, 101)
+        blur = cv2.bilateralFilter(img, d=-1, sigmaColor=50, sigmaSpace=3)
+        
+        #blur = cv2.adaptiveThreshold(img, maxValue=)
+
+        adaptativeThreshold = cv2.adaptiveThreshold(blur, C=-5, blockSize=31, maxValue=1, adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, thresholdType=cv2.THRESH_BINARY)
+
+        val, globalThreshold = cv2.threshold(blur, thresh=0, maxval=1, type=cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+        mask = adaptativeThreshold * globalThreshold
+
+        #out = cv2.GaussianBlur(out, ksize=(0, 0), sigmaX=2.0)
+
+        #out = blur[:] - (out[:] + 1)
+
+        #out = (out.astype(np.float32)/255) * blur
+
+        #out = np.where(out > 200, out, 0)
+        #out = out*255
+        #out = blur*0.5 + (out * blur)*0.5
 
         # CannyAccThresh = cv2.threshold(img ,0,255, cv2.THRESH_BINARY|cv2.THRESH_OTSU)
 
         # CannyThresh = 0.1 * CannyAccThresh
         
-        edges = cv2.Canny(blur, 75, 100)
+        #edges = cv2.Canny(out, 75, 100, 5)
         
         '''blur = cv2.bilateralFilter(img, 9, 75, 75)
         thr = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 31, 2)
@@ -36,7 +56,7 @@ def main():
         fechamento = cv2.morphologyEx(abertura, cv2.MORPH_CLOSE, kernel, iterations=1)'''
 
 
-        cv2.imwrite(f'test{nome}.png', edges)
+        cv2.imwrite(f'test{nome}.png', mask*255)
         
 
 if __name__ == '__main__':
